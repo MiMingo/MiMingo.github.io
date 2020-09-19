def6347b995252d1d34a3480cf6a5ebba49c43b3
MiMingo <wernertn@umich.edu>
2020-09-19 01:11:43 -0400

    add randomness to gradient position

--- mingo/src/app/pages/main/main.component.ts
+++ mingo/src/app/pages/main/main.component.ts
@@ -11,10 +11,13 @@ export class MainComponent implements OnInit {
 
   ngOnInit(): void {
     let time = 0;
+    let x = 50;
+    let y = 50;
     setInterval(() => {
       time = time + .5 % 360;
-      console.log();
-      this.renderer.setStyle(this.el.nativeElement, 'background', `conic-gradient(from ${time}deg, #2c2c54, #b33939`);
+      if (Math.floor(Math.random() * 1000) % 300 === 0) { x = Math.random() * 100; }
+      if (Math.floor(Math.random() * 1000) % 650 === 0) { y = Math.random() * 100; }
+      this.renderer.setStyle(this.el.nativeElement, 'background', `conic-gradient(from ${time}deg at ${x}% ${y}%, #2c2c54, #b33939`);
     }, 10);
   }
 
